fcaf3acf8aec685809e37951adacb0b83364ca86
ImSeventy <ahmedtop281972@gmail.com>
2022-11-24 13:51:57 +0200

    subjects routes protection

--- backend/routers/subjects.py
+++ backend/routers/subjects.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Depends
+from lib.authentication.authentication import oauth2_scheme, get_user
 from lib.database.manager import DataBaseManager
 from lib.exceptions.subjects import ClassAlreadyHasSubject, MajorNotFound, SubjectNotFound
 from lib.checks import checks
@@ -11,12 +12,16 @@ subjects = APIRouter(
 )
 
 @subjects.get("/class", response_model=list[SubjectOut], status_code=status.HTTP_200_OK)
-async def query_class_subjects(major_id: int, grade: int):
+async def query_class_subjects(major_id: int, grade: int, token: str = Depends(oauth2_scheme)):
+    _ = await get_user("admin", "student", token=token)
+
     return await DataBaseManager().query_class_subjects(major_id, grade)
 
 
 @subjects.get("/{id}", response_model=SubjectOut, status_code=status.HTTP_200_OK)
-async def get_subject(id: int):
+async def get_subject(id: int, token: str = Depends(oauth2_scheme)):
+    _ = await get_user("admin", "student", token=token)
+
     subject = await DataBaseManager().get_subject(id)
     if subject is None:
         raise SubjectNotFound()
@@ -25,7 +30,9 @@ async def get_subject(id: int):
 
 
 @subjects.post("/", response_model=SubjectOut, status_code=status.HTTP_201_CREATED)
-async def create_subject(subject: SubjectIn):
+async def create_subject(subject: SubjectIn, token: str = Depends(oauth2_scheme)):
+    _ = await get_user("admin", token=token)
+
     major = await DataBaseManager().get_major(subject.major_id)
     if major is None:
         raise MajorNotFound()
@@ -38,7 +45,9 @@ async def create_subject(subject: SubjectIn):
 
 
 @subjects.put("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_subject(id: int, new_subject: SubjectEdit):
+async def update_subject(id: int, new_subject: SubjectEdit, token: str = Depends(oauth2_scheme)):
+    _ = await get_user("admin", token=token)
+
     if not await checks.subject_exists(id):
         raise SubjectNotFound()
     
@@ -46,7 +55,9 @@ async def update_subject(id: int, new_subject: SubjectEdit):
 
 
 @subjects.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_subject(id: int):
+async def delete_subject(id: int, token: str = Depends(oauth2_scheme)):
+    _ = await get_user("admin", token=token)
+
     if not await checks.subject_exists(id):
         raise SubjectNotFound()
 
